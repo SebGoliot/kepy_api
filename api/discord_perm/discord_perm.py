24b24e9b3e23f82275aae10bfe1709707ea76276
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-04-22 11:18:18 +0200

    Added get_base_permission function logic
    test OK

--- api/discord_perm/discord_perm.py
+++ api/discord_perm/discord_perm.py
@@ -1,3 +1,4 @@
+from .discord_perm_const import *
 
 def get_base_permissions(guild: dict, member: dict) -> int:
     """This function gets the base permissions of a guild, this is used to
@@ -11,4 +12,26 @@ def get_base_permissions(guild: dict, member: dict) -> int:
         int: The base permission value for this member based on his roles
     """
 
-    return 0
+    # if the member is the owner -> return administrator permission
+    if member['user']['id'] == guild['owner_id']:
+        return ADMINISTRATOR
+
+    # get @everyone role as base permission
+    permissions = 0
+    for role in guild['roles']:
+        if role['id'] == guild['id']:
+            permissions = int(role['permissions'])
+            break
+
+    # get member base roles permissions
+    for member_role in member['roles']:
+        for guild_role in guild['roles']:
+            if guild_role['id'] == member_role:
+                permissions |= int(guild_role['permissions'])
+
+    # if admin -> return admin
+    if permissions & ADMINISTRATOR == ADMINISTRATOR:
+        return ADMINISTRATOR
+
+    return permissions
+
