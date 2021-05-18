8ca12239ddca1f29b5701e32e5eb8bf419b6b230
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-05-18 16:10:24 +0200

    Added sentry monitoring and .env sample file

--- kepy/settings.py
+++ kepy/settings.py
@@ -13,8 +13,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from os import environ as os_environ
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 load_dotenv()
+
+SENTRY_DSN = os_environ["SENTRY_DSN"]
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
 
 API_BASE_URL = os_environ["API_BASE_URL"]
 KEPY_TOKEN = os_environ["KEPY_TOKEN"]
