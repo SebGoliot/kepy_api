1c08b4a72bad1edfd2c479f90d0d6085077b2bf3
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-09-30 19:17:12 +0200

    Added interaction request signature test and logic
    Added required const in settings, added dependency in requirements

--- api/views/interactions.py
+++ api/views/interactions.py
@@ -1,12 +1,37 @@
-from django.http.response import HttpResponseBadRequest, JsonResponse
+from django.http.response import HttpResponseBadRequest, JsonResponse, HttpResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from django.conf import settings
+
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
 
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def interactions(request):
-    if request.data.get("type") == "1":  # If the interaction is a PING
+
+    PING                = "1"
+    APPLICATION_COMMAND = "2"
+    MESSAGE_COMPONENT   = "3"
+
+    verify_key = VerifyKey(bytes.fromhex(settings.KEPY_PUBLIC_KEY))
+    signature = request.headers["X-Signature-Ed25519"]
+    timestamp = request.headers["X-Signature-Timestamp"]
+    body = request.data
+
+    try:
+        verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+    except BadSignatureError:
+        return HttpResponse("invalid request signature", status=401)
+
+    interaction_type = str(request.data.get("type"))
+
+    if interaction_type == PING:
         return JsonResponse(data={"type": 1})
-    
+    elif interaction_type == APPLICATION_COMMAND:
+        pass
+    elif interaction_type == MESSAGE_COMPONENT:
+        pass
+
     return HttpResponseBadRequest()
