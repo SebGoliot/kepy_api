c2609d78daa1b440a2040f91b75dec01b79e22bc
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-10-02 15:02:40 +0200

    Added the 'Mute author' app interaction handling

--- api/views/interactions.py
+++ api/views/interactions.py
@@ -6,6 +6,18 @@ from django.conf import settings
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
 
+from discord_perm.discord_perm_const import MANAGE_MESSAGES
+from discord_perm.discord_perm import has_permission
+
+from api.helpers.interactions import (
+    get_interaction_author_id,
+    get_interaction_author_permissions,
+    get_interaction_guild_id,
+    get_interaction_name,
+)
+from api.helpers.app_interactions import get_app_message_author
+from api.helpers.mute import create_mute
+
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
@@ -30,8 +42,41 @@ def interactions(request):
     if interaction_type == PING:
         return JsonResponse(data={"type": 1})
     elif interaction_type == APPLICATION_COMMAND:
-        pass
-    elif interaction_type == MESSAGE_COMPONENT:
-        pass
+        if int_name := get_interaction_name(body):
+            if interact := interact_bindings.get(int_name):
+                response = interact(body)
+                return JsonResponse(data={"content": response})
 
     return HttpResponseBadRequest()
+
+
+def mute_author(body) -> str:
+    """This function handles the mute app interaction and returns a message
+
+    Args:
+        body (dict): The interaction request body
+
+    Returns:
+        str: The returned message
+    """
+
+    if has_permission(get_interaction_author_permissions(body), MANAGE_MESSAGES):
+        muted = get_app_message_author(body)
+        create_mute(
+            guild_id=get_interaction_guild_id(body),
+            author_id=get_interaction_author_id(body),
+            muted_id=muted["id"],
+            duration=30 * 60,
+            reason="Muted with application command",
+        )
+        return _mute_success(muted["username"], 30, True)
+    return _mute_success("", 30, False)
+
+
+def _mute_success(muted_name: str, duration: int, success: bool):
+    if success:
+        return f"{muted_name} has been muted for {duration} minutes."
+    return "Seems like you are not allowed to do this.."
+
+
+interact_bindings = {"Mute author": mute_author}
